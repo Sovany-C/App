8e955b81b3be737bc29f5eb4646ac07ae848a7f2
Arthurchld <arthurhalaud1@gmail.com>
2025-05-28 19:08:25 +0200

    Realignement

--- src/main/java/org/openjfx/Pane/AtelierPane.java
+++ src/main/java/org/openjfx/Pane/AtelierPane.java
@@ -195,8 +195,13 @@ public class AtelierPane extends VBox {
         this.bt_operateur.setOnAction(evt -> {
             this.controleur.openOperateur();
         });
+        c++;
 
-        
+         this.bt_fiabilite = new Button("Fiabilite");
+         this.pane_saisiedesinfo.add(bt_fiabilite,c,l);
+         this.bt_fiabilite.setOnAction(evt -> {
+             this.controleur.openFiabilite();
+         });
         c++;
         l++;
 
@@ -206,11 +211,7 @@ public class AtelierPane extends VBox {
         });
         c++;
 
-         this.bt_fiabilite = new Button("Fiabilite");
-         this.pane_saisiedesinfo.add(bt_fiabilite,c,l);
-         this.bt_fiabilite.setOnAction(evt -> {
-             this.controleur.openFiabilite();
-         });
+        
 
         this.espace_affichage = new Pane();
         this.espace_affichage.setPrefSize(900, 600);
